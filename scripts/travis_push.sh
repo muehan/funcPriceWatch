ccf3b5ec1b3bd94b22f0e080a74bce7d6e23b7fb
Andre Mühle <tdc@gmx.ch>
2020-10-11 14:20:22 +0200

    add travis

---
+++ scripts/travis_push.sh
@@ -0,0 +1,19 @@
+echo "Travis push job"
+
+# Download dependencies and build
+dotnet publish functions
+
+# Update the stack
+case ${TRAVIS_BRANCH} in
+    master)
+        pulumi stack select dev
+        pulumi up --yes
+        ;;
+    production)
+        pulumi stack select dev
+        pulumi up --yes
+        ;;
+    *)
+        echo "No Pulumi stack associated with branch ${TRAVIS_BRANCH}."
+        ;;
+esac
