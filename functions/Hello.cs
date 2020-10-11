f1b6983822c49195fcf4da01638d11be987930e8
Andre Mühle <tdc@gmx.ch>
2020-10-11 16:50:24 +0200

    trigger action

--- functions/Hello.cs
+++ functions/Hello.cs
@@ -26,7 +26,7 @@ namespace Functions
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello my friend: {name}")
+                ? (ActionResult)new OkObjectResult($"Hello my friend, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
