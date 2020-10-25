696a446021b73c9194dbf2cff04a6edee166a821
André <tdc@gmx.ch>
2020-10-25 17:31:44 +0100

    return string for test

--- functions/DailyReport.cs
+++ functions/DailyReport.cs
@@ -1,8 +1,9 @@
-using System;
-using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace functions
@@ -10,11 +11,13 @@ namespace functions
     public static class DailyReport
     {
         [FunctionName("DailyReport")]
-        public static void Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation($"DailyReport function triggered");
+
+            return (ActionResult) new OkObjectResult($"Hello there");
         }
     }
 }
